47de99516a53a5de6a2a269920ed9baf69cc62ad
LeHanhJ <101995252+LeHanhJ@users.noreply.github.com>
2022-04-28 13:00:14 -0700

    practiced js files

---
+++ Basic_Javascript_Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,19 @@
+//window.alert("Hello, World!");
+//document.write("Hello World!");
+
+//var A = "This is a strong... I-I mean a string!";
+//document.write(A);
+
+//var B = "Click OK if you are cool";
+//window.alert(B);
+
+//var C = "And so the One said, \"I am The One, you aren\'t.\"";
+//document.write(C);
+
+//var D = " \"Don\'t fret,\" she said," + " \"just take care of yourself.\"";
+//document.write(D);
+
+//var flower="hibiscus", bird="nene", ocean="pacific", fish="humuhumunukunukuapua'a";
+//document.write(bird);
+
+document.write(4+3);
