ed4c81c8baa1b582987932d9716def0651495ce6
LeHanhJ <101995252+LeHanhJ@users.noreply.github.com>
2022-04-28 14:01:54 -0700

    Updated assignment for index.html on project 1

--- Basic_Javascript_Projects/Project1_expressions_alert/JS/main.js
+++ Basic_Javascript_Projects/Project1_expressions_alert/JS/main.js
@@ -1,21 +1,22 @@
 //window.alert("Hello, World!");
 //document.write("Hello World!");
 
-//var A = "This is a strong... I-I mean a string!";
-//document.write(A);
+var A = "This is a strong... I-I mean a string!";
 
 var B = "Click OK if you are cool"; //This assigns B to variable
 window.alert(B); //This makes variable B as a window alert
 
-//var C = "And so the One said, \"I am The One, you aren\'t.\"";
-//document.write(C);
+var C = "And so the One said, \"I am The One, you aren\'t.\"";
+document.write(C);
 
-var D = " \"Don\'t fret,\" she said," + " \"just take care of yourself.\""; //This concatenates two strongs together and aassigns them to variable D
-document.write(D); //This prints variable D on the webpage
+var D = " \"Don\'t fret,\" she said," + " \"just take care of yourself.\"" + " " + C + " " + A; //This concatenates two strongs together and aassigns them to variable D
+
+var E = D.fontcolor("green"); //This set variable E as variable D with a font color change to green
+document.write(E); //This prints variable E on the webpage
 
 //var flower="hibiscus", bird="nene", ocean="pacific", fish="humuhumunukunukuapua'a";
 //document.write(bird);
 
 document.write(4*3); //This prints and processes 4*3
 
-D.fontcolor(green); //Based on Google and VSC, it seems like variable.fontcolor() is deprecated
+
