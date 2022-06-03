74f3a38edc8723984f71755b7d2dce8c3e3cbbf8
LeHanhJ <101995252+LeHanhJ@users.noreply.github.com>
2022-06-03 16:49:21 -0700

    changed code so calculator.html works

--- Calculator_Project/JS/calculator_js.js
+++ Calculator_Project/JS/calculator_js.js
@@ -1,102 +1,102 @@
 //creates an object to keep track of values
-const calculator = {
+const Calculator = {
     //this displays 0 on the screen
-    Display_value: '0',
+    Display_Value: '0',
     //this will hold the first operand for any expressions, we set it to null for now
-    First_operand: null,
+    First_Operand: null,
     //this checks whether or not the second operand has been input
-    Wait_second_operand: false,
+    Wait_Second_Operand: false,
     //this will hold the operator, we will set it to null for now
     operator: null,
 };
 
 //this modifies values each time a button is clicked
-function Input_digit(digit) {
-    const { Display_value, Wait_second_operand } = calculator;
+function Input_Digit(digit) {
+    const { Display_Value, Wait_Second_Operand } = Calculator;
     //we are checking to see if Wait_second_operand is true and set Display_value to the key that was clicked
-    if (Wait_second_operand === true){
-        calculator.Display_value = digit;
-        calculator.Wait_second_operand=false;
+    if (Wait_Second_Operand === true){
+        Calculator.Display_Value = digit;
+        Calculator.Wait_Second_Operand=false;
     } else {
     //this overwrites Display_value if the current value is 0
     //otherwise it adds onto it
-        calculator.Display_value = Display_value === '0' ? digit : Display_value + digit ;
+        Calculator.Display_Value = Display_Value === '0' ? digit : Display_Value + digit ;
     }
 }
 
 //this section handles decimal points
-function Input_decimal(dot) {
+function Input_Decimal(dot) {
     //this ensures that accidental clicking of the decimal point doesn't cause bugs in your operation
-    if (calculator.Wait_second_operand === true) return;
-    if (!calculator.Display_value.includes(dot)) {
+    if (Calculator.Wait_Second_Operand === true) return;
+    if (!Calculator.Display_Value.includes(dot)) {
         //we are saying that if the Display_value does not contain a decimal point
         //we want to add a decimal point
-        calculator.Display_value += dot;
+        Calculator.Display_Value += dot;
     }
 }
 
 
 
 //this section handles operators
-function Handle_operator(Next_operator) {
-    const { First_operand, Display_value, operator } = calculator
+function Handle_Operator(Next_Operator) {
+    const { First_Operand, Display_Value, operator } = Calculator
     //when an operator key is pressed, we convert the current number
     //displayed on the screen to a number and then store the result in 
     //Calculator.First_operand if it doesn't already exist
-    const Value_of_Input = parseFloat(Display_value);
+    const Value_of_Input = parseFloat(Display_Value);
     //checks if an operator already exists and if Wait_second_operand is true,
     //then updates the operator and exits from the function
-    if (operator && calculator.Wait_second_operand) {
-        calculator.operator = Next_operator;
+    if (operator && Calculator.Wait_Second_Operand) {
+        Calculator.operator = Next_Operator;
         return;
     }
-    if (First_operand == null) {
-        calculator.First_operand = Value_of_Input;
+    if (First_Operand == null) {
+        Calculator.First_Operand = Value_of_Input;
     } else if (operator) {//checks if an operator already exists
-    const Value_now = First_operand || 0;
+    const Value_Now = First_Operand || 0;
     //if the operator exists, property lookup is performed for the operator
     //in the Perform_calculation object and the function that matches the operator is executed
-    let result = Perform_calculation[operator] (Value_now,Value_of_Input);
+    let result = Perform_Calculation[operator] (Value_Now, Value_of_Input);
     //here we add a fixed amount of numbers after the decimal
     result = Number(result).toFixed(9)
     //this will remove any trailing 0s
     result = (result * 1).toString()
-    calculator.Display_value = parseFloat(result);
-    calculator.First_operand = parseFloat(result);
+    Calculator.Display_Value = parseFloat(result);
+    Calculator.First_Operand = parseFloat(result);
 
     }
 
-    calculator.Wait_second_operand = true;
-    calculator.First_operand = Next_operator;
+    Calculator.Wait_Second_Operand = true;
+    Calculator.operator = Next_Operator;
 
 }
 
-    const Perform_calculation = {
-        '/': (First_operand,Second_operand) => First_operand / Second_operand,
+    const Perform_Calculation = {
+        '/': (First_Operand,Second_Operand) => First_Operand / Second_Operand,
 
-        '*': (First_operand,Second_operand) => First_operand * Second_operand,
+        '*': (First_Operand,Second_Operand) => First_Operand * Second_Operand,
 
-        '+': (First_operand,Second_operand) => First_operand + Second_operand,
+        '+': (First_Operand,Second_Operand) => First_Operand + Second_Operand,
 
-        '-': (First_operand,Second_operand) => First_operand - Second_operand,
+        '-': (First_Operand,Second_Operand) => First_Operand - Second_Operand,
 
-        '=': (First_operand,Second_operand) => Second_operand
+        '=': (First_Operand,Second_Operand) => Second_Operand
     };
 
-    function Calculator_reset() {
-        calculator.Display_value = '0';
-        calculator.First_operand = null;
-        calculator.Wait_second_operand = false;
-        calculator.operator = null;
+    function Calculator_Reset() {
+        Calculator.Display_Value = '0';
+        Calculator.First_Operand = null;
+        Calculator.Wait_Second_Operand = false;
+        Calculator.operator = null;
     }
 
     //this function updates the screen with the contents of Display_value
-    function Update_display() {
+    function Update_Display() {
         const display = document.querySelector('.calculator-screen');
-        display.value = calculator.Display_value;
+        display.value = Calculator.Display_Value;
     }
 
-    Update_display();
+    Update_Display();
 
     //this section monitors button clicks
     const keys = document.querySelector('.calculator-keys');
@@ -110,24 +110,24 @@ function Handle_operator(Next_operator) {
         }
 
         if (target.classList.contains('operator')) {
-            Handle_operator(target.value);
-            Update_display();
+            Handle_Operator(target.value);
+            Update_Display();
             return;
         }
 
         if (target.classList.contains('decimal')) {
-            Input_decimal(target.value);
-            Update_display();
+            Input_Decimal(target.value);
+            Update_Display();
             return;
         }
 
         //ensures that AC clears the numbers from the calculator
         if (target.classList.contains('all-clear')) {
-            Calculator_reset();
-            Update_display();
+            Calculator_Reset();
+            Update_Display();
             return;
         }
 
-    Input_digit(target.value);
-    Update_display();
+    Input_Digit(target.value);
+    Update_Display();
 })
